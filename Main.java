990be5f3d389fc8cd319efb63dd8746047184e19
Igor Efimenko <igor1964egor@yandex.ru>
2021-03-12 18:14:48 +1000

    Update

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
-  public static void main(String[] args)
-  {
-    int amount = 3000; //Стоимость билета
-    int count = 20; // за каждые 20 рублей, потраченные на билет, начисляется 1 миля
-    int Bonus = amount / count; // бонус в милях
-      System.out.print(Bonus);System.out.print("миль");}
-  }
+    public static void main(String[] args) {
+        int amount = 3000; //Стоимость билета
+        int count = 20; // за каждые 20 рублей, потраченные на билет, начисляется 1 миля
+        int bonus = amount / count; // бонус в милях
+        System.out.print(bonus);
+        System.out.print("миль");
+    }
+}
