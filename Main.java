212c3a5b193b8e2b67b139b4d5e71b694266b58d
Igor Efimenko <igor1964egor@yandex.ru>
2021-03-12 13:54:20 +1000

    Java Code

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args)
+  {
+    int amount = 3000; //Стоимость билета
+    int count = 20; // за каждые 20 рублей, потраченные на билет, начисляется 1 миля
+    int Bonus = amount / count; // бонус в милях
+      System.out.print(Bonus);System.out.print("миль");}
+  }
